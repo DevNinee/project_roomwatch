dacbe5ffe8acdd25fdc68dfd240445860a7efeff
Dev Voyager <86367089+DevNinee@users.noreply.github.com>
2024-06-25 23:38:15 -0300

    Create var.js

---
+++ var.js
@@ -0,0 +1,18 @@
+var movielist = [
+  "https://m.media-amazon.com/images/I/51O-d4k-ACL._AC_UF1000,1000_QL80_.jpg",
+  "https://www.cecierj.edu.br/wp-content/uploads/2014/02/pijama.jpg",
+  "https://images.justwatch.com/poster/208739380/s718/eduardo-maos-de-tesoura.%7Bformat%7D",
+  "https://br.web.img3.acsta.net/pictures/18/10/05/18/29/2308941.jpg",
+  "https://pm1.aminoapps.com/7149/51eded039acf0219f7b07f51470c747f26f5b60fr1-760-1022v2_00.jpg",
+  "https://br.web.img3.acsta.net/pictures/21/10/13/21/56/4755833.jpg",
+  "https://m.media-amazon.com/images/M/MV5BMzI0NmVkMjEtYmY4MS00ZDMxLTlkZmEtMzU4MDQxYTMzMjU2XkEyXkFqcGdeQXVyMzQ0MzA0NTM@._V1_FMjpg_UX1000_.jpg",
+  "https://media.fstatic.com/UM7NitgsEvpJoGtkdVC4lZYUbXw=/322x478/smart/filters:format(webp)/media/movies/covers/2020/01/82889882_2558066531138431_3022026817251835904_n.jpg",
+  "https://cinepop.com.br/wp-content/uploads/2023/07/frio-nos-ossos-poster.jpg.webp",
+  "https://assets.b9.com.br/wp-content/uploads/2021/09/sCoesZnspv1yuWuGdO0Re7e9AzR-1610x2415.jpg",
+  "https://br.web.img2.acsta.net/pictures/23/04/12/11/07/5761666.jpg",
+  "https://www.justwatch.com/images/poster/139784210/s332/donnie-darko"
+];
+
+for (var e = 0; e < movielist.length; e++) {
+  document.write("<img src=" + movielist[e] + ">");
+}
